fc743bb9dcda0b8cc34ceea9feab2b389cacc5c7
Dongwoo Pae <dongwoopae@gmail.com>
2019-11-07 11:34:13 +0900

    Initial Commit

---
+++ Objective-C Playground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C Playground
+//
+//  Created by Dongwoo Pae on 11/7/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
